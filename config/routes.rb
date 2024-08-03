5dc72e2662d3417d22314ee63d194ed96680d777
star_ <star_berry2s@yahoo.co.jp>
2024-08-03 12:35:41 +0900

    ヘッダーフッター

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'staticpages#top'
-  
+  root 'static_pages#top'
+
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
