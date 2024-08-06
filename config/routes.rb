0a6a82af01690311b4c686173b4b1f2ba9b0b8ae
star_ <star_berry2s@yahoo.co.jp>
2024-08-06 16:54:25 +0900

    新規登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
 
+  resources :users, only: %i[new create]
+
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
