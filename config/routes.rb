d040c5e65a18348804ba002fbdceca40fddc17d4
star_ <star_berry2s@yahoo.co.jp>
2024-08-19 19:17:21 +0900

    Google認証0819

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,19 @@ Rails.application.routes.draw do
   
   # ユーザー作成のためのPOSTリクエストのルーティング
   post 'users', to: 'users#create', as: 'users'
-  
+
+  # ログインログアウト
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+
+  # パスワードリセット
+  resources :password_resets, only: %i[new create edit update]
+
+  # Google 認証
+  post "oauth/callback" => "oauths#callback"
+  get "oauth/callback" => "oauths#callback"
+  get "oauth/:provider" => "oauths#oauth", as: :auth_at_provider
 
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -20,4 +29,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # 開発環境でのメール確認
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_06_045143) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_18_015642) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authentications", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "provider", null: false
+    t.string "uid", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["provider", "uid"], name: "index_authentications_on_provider_and_uid"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
@@ -21,13 +30,18 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_06_045143) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
     t.string "email", null: false
     t.string "crypted_password"
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
+    t.integer "access_count_to_reset_password_page", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
