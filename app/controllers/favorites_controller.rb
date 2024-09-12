ccfc3fba68eef6594f29f95f0d87a12c0fc5b5b0
star_ <star_berry2s@yahoo.co.jp>
2024-09-12 16:41:35 +0900

    お気に入り機能

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,18 @@
 class FavoritesController < ApplicationController
   def index
+    @q = Post.ransack(params[:q])
+    @favorite_posts = current_user.favorite_posts.includes(:user).order(created_at: :desc)
+    @favorite_posts = @favorite_posts.page(params[:page]).per(12)
+    @posts = @q.result(distinct: true).includes(:user).order(created_at: :desc).page(params[:page])
   end
 
   def create
+    @post = Post.find(params[:post_id])
+    current_user.favorite(@post)
   end
 
   def destroy
+    @post = current_user.favorites.find(params[:id]).post
+    current_user.unfavorite(@post)
   end
 end
