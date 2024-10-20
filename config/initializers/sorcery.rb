8461c5c7f3824fa2e3ff093c253490486099257a
star_ <star_berry2s@yahoo.co.jp>
2024-10-20 10:54:41 +0900

    Googleログイン修正

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -5,12 +5,7 @@ Rails.application.config.sorcery.configure do |config|
   config.external_providers = [:google]
   config.google.key = Rails.application.credentials.dig(:google, :google_client_id)
   config.google.secret = Rails.application.credentials.dig(:google, :google_client_secret)
-
-  # 環境によってcallback_urlを設定
-  config.google.callback_url = Rails.env.production? ? 
-                              "https://trainspotter.onrender.com/oauth/callback?provider=google" : 
-                              "http://localhost:3000/oauth/callback?provider=google"
-
+  config.google.callback_url = Settings.sorcery[:google_callback_url]
   config.google.user_info_mapping = { email: 'email', name: 'name' }
 
   config.user_config do |user|
